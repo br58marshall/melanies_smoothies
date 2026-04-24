12edf924979d97df26ee6073510ba4e0083612d8
br58marshall <149958400+br58marshall@users.noreply.github.com>
2026-04-24 14:56:12 -0600

    Update streamlit_app.py
    Making the API call repeat (watermelon) for each ingredient

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages.
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app.
@@ -25,6 +26,8 @@ if ingredients_list and name_on_order:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     # st.write(ingredients_string)
 
@@ -50,9 +53,3 @@ if ingredients_list and name_on_order:
     #     else:
     #         session.sql(my_insert_stmt).collect()
     #         st.success('Your Smoothie is ordered!', icon="✅")        
-
-# New section to display smoothiefroot nutrition information
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
